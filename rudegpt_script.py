6e79923636a613a3a77dd3cd0d278e0b69d2c13a
megapoisin <150597701+megapoisin@users.noreply.github.com>
2023-11-12 08:12:14 -0500

    Delete rudegpt_script.py

--- rudegpt_script.py
+++
@@ -1,4 +0,0 @@
-from openai import OpenAI
-client = OpenAI(api_key="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")
-my_assistant = client.beta.assistants.retrieve("asst_n2YNcMBAYvP5Gw56jXv5U1Q3")
-print(my_assistant)
